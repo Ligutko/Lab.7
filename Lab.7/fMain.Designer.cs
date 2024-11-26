3b09d37133ee6c26303b67b26aea1910265a0032
ligutko <desantvitaliy@gmail.com>
2024-11-26 19:44:31 +0200

    Add project files.

---
+++ Lab.7/fMain.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Lab._7
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.directorySearcher2 = new System.DirectoryServices.DirectorySearcher();
+            this.directorySearcher3 = new System.DirectoryServices.DirectorySearcher();
+            this.directorySearcher4 = new System.DirectoryServices.DirectorySearcher();
+            this.directorySearcher5 = new System.DirectoryServices.DirectorySearcher();
+            this.directorySearcher6 = new System.DirectoryServices.DirectorySearcher();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.buttonCalculateAverage = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 348);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Змінна X1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Змінна X2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Result of Calculation  Y";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // directorySearcher1
+            // 
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // directorySearcher2
+            // 
+            this.directorySearcher2.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher2.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher2.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // directorySearcher3
+            // 
+            this.directorySearcher3.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher3.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher3.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // directorySearcher4
+            // 
+            this.directorySearcher4.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher4.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher4.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // directorySearcher5
+            // 
+            this.directorySearcher5.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher5.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher5.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // directorySearcher6
+            // 
+            this.directorySearcher6.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher6.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher6.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(172, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Clean data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 5, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(255, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(293, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(194, 62);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(255, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(240, 216);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(209, 22);
+            this.textBox4.TabIndex = 4;
+            // 
+            // buttonCalculateAverage
+            // 
+            this.buttonCalculateAverage.Location = new System.Drawing.Point(407, 348);
+            this.buttonCalculateAverage.Name = "buttonCalculateAverage";
+            this.buttonCalculateAverage.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculateAverage.TabIndex = 5;
+            this.buttonCalculateAverage.Text = "Average";
+            this.buttonCalculateAverage.UseVisualStyleBackColor = true;
+            this.buttonCalculateAverage.Click += new System.EventHandler(this.buttonCalculateAverage_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(521, 348);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "function";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.buttonCalculateAverage);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "fMain";
+            this.Text = "Laba #7";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher2;
+        private System.DirectoryServices.DirectorySearcher directorySearcher3;
+        private System.DirectoryServices.DirectorySearcher directorySearcher4;
+        private System.DirectoryServices.DirectorySearcher directorySearcher5;
+        private System.DirectoryServices.DirectorySearcher directorySearcher6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button buttonCalculateAverage;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Lab.7/fMain.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab._7
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox3.Text;
+            string b = textBox2.Text;
+
+            int a_1 = Convert.ToInt32(a);
+            int b_1 = Convert.ToInt32(b);
+            int ab = a_1 + b_1;
+
+            textBox4.Text = ab.ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //string l1 = textBox3.Text;
+            //string l2 = textBox2.Text;
+
+            //int l1_1 = Convert .ToInt32(l1);
+            //int l2_1 = Convert.ToInt32(l2);
+            //int l1l2 = l1_1 + l2_1;
+
+            //textBox4.Text = l1l2.ToString();
+            Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+         
+            string a = textBox3.Text;
+            string b = textBox2.Text;
+
+            int a_1 = Convert.ToInt32(a);
+            int b_1 = Convert.ToInt32(b);
+
+           
+            a_1 = 0;
+            b_1 = 0;
+
+            textBox3.Text = a_1.ToString();
+            textBox2.Text = b_1.ToString();
+            textBox4.Text = ""; 
+
+        }
+        //private void buttonCalculateAverage_Click(object sender, EventArgs e)
+        //{
+        //    try
+        //    {
+        //        // Отримання значень з текстових полів
+        //        string x1 = textBox3.Text;
+        //        string x2 = textBox2.Text;
+
+        //        // Перетворення на числа
+        //        double x1_value = Convert.ToDouble(x1);
+        //        double x2_value = Convert.ToDouble(x2);
+
+        //        // Обчислення середнього арифметичного
+        //        double average = (x1_value + x2_value) / 2;
+
+        //        // Виведення результату у текстове поле або повідомлення
+        //        textBox4.Text = average.ToString();
+        //    }
+        //    catch (FormatException)
+        //    {
+        //        // Обробка помилки, якщо введено некоректні дані
+        //        MessageBox.Show("Будь ласка, введіть числові значення у поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //    }
+        //}
+
+        private void buttonCalculateAverage_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                string x1 = textBox3.Text;
+                string x2 = textBox2.Text;
+
+               
+                double x1_value = Convert.ToDouble(x1);
+                double x2_value = Convert.ToDouble(x2);
+
+                
+                double average = (x1_value + x2_value) / 2;
+
+                textBox4.Text = average.ToString();
+            }
+            catch (FormatException)
+            {
+               
+                MessageBox.Show("Будь ласка, введіть числові значення у поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string x1_text = textBox3.Text;
+                string x2_text = textBox2.Text;
+
+                double x1 = Convert.ToDouble(x1_text);
+                double x2 = Convert.ToDouble(x2_text);
+
+                double numerator, denominator, y;
+
+                if (x1 == 0)
+                {
+                    MessageBox.Show("x1 не може дорівнювати 0, але функція продовжить виконання з x1 = 0.1.", 
+                        "Попередження", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    x1 = 0.1;
+                }
+
+                if (Math.Cos(x2) == 0)
+                {
+                    MessageBox.Show("cos(x2) не може дорівнювати 0, але функція продовжить виконання з x2 = 1.",
+                        "Попередження", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    x2 = 1;
+                }
+
+                numerator = Math.Sqrt(Math.Pow(Math.Cos(x1), 3) + x2);
+                denominator = Math.Pow(x1, 13) + (3 / Math.Cos(x2));
+                y = numerator / denominator;
+
+                textBox4.Text = y.ToString("F4");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Будь ласка, введіть числові значення у поля!", "Помилка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Виникла помилка: {ex.Message}", "Помилка",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+            //try
+            //{
+            //    // Отримання значень з текстових полів
+            //    string x1 = textBox3.Text;
+            //    string x2 = textBox2.Text;
+
+            //    // Перетворення на числа
+            //    double x1_value = Convert.ToDouble(x1);
+            //    double x2_value = Convert.ToDouble(x2);
+
+            //    // Обчислення середнього арифметичного
+            //    double average = (x1_value + x2_value) / 2;
+
+            //    // Форматування результату до 4 знаків після коми
+            //    textBox4.Text = average.ToString("F4");
+            //}
+            //catch (FormatException)
+            //{
+            //    // Обробка помилок введення
+            //    MessageBox.Show("Будь ласка, введіть числові значення у поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+//using System;
+//using System.Windows.Forms;
+
+//namespace Lab._7
+//{
+//    public partial class fMain : Form
+//    {
+//        public fMain()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            try
+//            {
+//                string a = textBox3.Text;
+//                string b = textBox2.Text;
+
+//                int a_1 = Convert.ToInt32(a);
+//                int b_1 = Convert.ToInt32(b);
+//                int ab = a_1 + b_1;
+
+//                textBox4.Text = ab.ToString();
+//            }
+//            catch (FormatException)
+//            {
+//                MessageBox.Show("Будь ласка, введіть цілі значення!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//            }
+//        }
+
+//        private void button2_Click(object sender, EventArgs e)
+//        {
+//            textBox3.Text = "";
+//            textBox2.Text = "";
+//            textBox4.Text = "";
+//        }
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+//            textBox3.Text = ""; // Поле X1 порожнє
+//            textBox2.Text = ""; // Поле X2 порожнє
+//            textBox4.Text = ""; // Поле результату порожнє
+//        }
+
+//        private void button3_Click(object sender, EventArgs e)
+//        {
+//            Application.Exit();
+//        }
+
+//        private void buttonCalculateAverage_Click(object sender, EventArgs e)
+//        {
+//            try
+//            {
+//                string x1 = textBox3.Text;
+//                string x2 = textBox2.Text;
+
+//                double x1_value = Convert.ToDouble(x1);
+//                double x2_value = Convert.ToDouble(x2);
+
+//                double average = (x1_value + x2_value) / 2;
+
+//                textBox4.Text = average.ToString("F4");
+//            }
+//            catch (FormatException)
+//            {
+//                MessageBox.Show("Будь ласка, введіть числові значення у поля!", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//            }
+//        }
+//    }
+//}
